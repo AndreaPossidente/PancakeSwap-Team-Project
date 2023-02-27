8f66059241d96ea8ec966089cc309080125ee018
Matteo Spinelli <matteospinelli10@gmail.com>
2023-02-27 16:32:57 +0100

    complete win section + dark theme

--- js/components/cake.js
+++ js/components/cake.js
@@ -2,4 +2,4 @@ import { increaseNumWithCommas } from "../utils/increaseNum.js";
 
 const cakeStats = document.querySelectorAll(".cake-stats-item-value > span");
 
-increaseNumWithCommas(cakeStats);
+increaseNumWithCommas(cakeStats, 3);
