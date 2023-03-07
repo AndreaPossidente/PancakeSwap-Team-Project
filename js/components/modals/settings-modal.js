5bb0262ca188ff67fce5847e1a2050fcabf32912
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-07 11:17:16 +0100

    add animation

--- js/components/modals/settings-modal.js
+++ js/components/modals/settings-modal.js
@@ -1,8 +1,22 @@
 const modal = document.querySelector("#settings-modal")
 const options = document.querySelectorAll(".nav-options")
 
-options.forEach((el) => {
-    el.addEventListener("click", () => {
+function close(element){
+    const body = document.querySelector("main")
+    const svg = document.querySelector(".settings-modal-header .settings-modal-close")
+    const util = []
+    util.push(body, svg)
+    util.forEach((el) => {
+        el.addEventListener("click", () => {
+            element.classList.remove("active")
+        })
+    });
+}
+
+options.forEach((option) => {
+    option.addEventListener("click", () => {
         modal.classList.toggle("active")
     })
 })
+
+close(modal)
