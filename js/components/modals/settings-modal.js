0588d2a553bf0eed66a8f46b5ba0364cf2ab391c
MatteoSpinelli <matteospinelli10@gmail.com>
2023-03-08 01:18:54 +0100

    fix animations

--- js/components/modals/settings-modal.js
+++ js/components/modals/settings-modal.js
@@ -1,15 +1,17 @@
 const modal = document.querySelector("#settings-modal")
 const options = document.querySelectorAll(".nav-options")
+const opacity = document.querySelector(".modal-fade")
 
-function close(element){
-    const body = document.querySelector("main")
+function close(element) {
     const svg = document.querySelector(".settings-modal-header .settings-modal-close")
     const util = []
-    util.push(body, svg)
+    util.push(opacity, svg)
     util.forEach((el) => {
         el.addEventListener("click", () => {
             element.classList.remove("active")
             element.classList.add("deactive")
+            opacity.classList.remove("active")
+            opacity.classList.add("deactive")
         })
     });
 }
@@ -18,6 +20,8 @@ options.forEach((option) => {
     option.addEventListener("click", () => {
         modal.classList.remove("deactive")
         modal.classList.toggle("active")
+        opacity.classList.remove("deactive")
+        opacity.classList.toggle("active")
     })
 })
 
