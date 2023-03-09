492121dbb66b51cdfaf920ef75cbaeeca84b14ac
Matteo Spinelli <matteospinelli10@gmail.com>
2023-03-09 17:59:05 +0100

    add buttons switchers

--- js/components/modals/settings-modal.js
+++ js/components/modals/settings-modal.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector("#settings-modal")
 const options = document.querySelectorAll(".nav-options")
 const opacity = document.querySelector(".modal-fade")
+const btns = document.querySelectorAll("#settings-modal .settings-modal-container .settings-modal-main .settings-modal-options:last-child button")
+const swtichers = document.querySelectorAll("#settings-modal .settings-modal-container .settings-modal-main .settings-modal-options .color-scheme-switcher.preserve .color-scheme-switcher-bubble")
 
 function close(element) {
     const svg = document.querySelector(".settings-modal-header .settings-modal-close")
@@ -26,3 +28,25 @@ options.forEach((option) => {
 })
 
 close(modal)
+
+// buttons logic
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        for (let buttons of btns){
+            buttons.classList.add("deactive")
+        }
+        btn.classList.toggle("deactive")
+    })
+})
+
+//switcher logic
+
+swtichers.forEach((switcher) => {
+    switcher.addEventListener("click", () => {
+        switcher.classList.toggle("active")
+        switcher.classList.toggle("deactive")
+        switcher.parentNode.classList.toggle("active")
+        switcher.parentNode.classList.toggle("deactive")
+    })
+})
