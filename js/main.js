20b08220d158ab76be6be8090ea8fc800c6269dd
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-14 00:01:14 +0100

    fix-footer

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ import "./components/nav.js";
 import "./components/modals/connect-wallet.js";
 import "./components/modals/settings-modal.js";
 import "./components/chart.js";
-import "./utils/svg-footer.js";
 import drawChart from "./components/chart.js";
 
 drawChart("cake", "bnb");
